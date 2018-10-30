9c710bf8a975e124ecd3f105610d0dfa81432ae7
William Cuttrell <wcuttrell@gmail.com>
2018-10-30 16:56:23 -0400

    fixed age and name

--- action.js
+++ action.js
@@ -1,15 +1,37 @@
-function checkEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+console.log("start");
+document.getElementById('email').onkeyup = function() {
+    var email = document.getElementById('email').value;
+    var re = new RegExp('/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/');
 
-}
-
-function checkAge() {
-    var regEx = new RegExp('^(([0-9][1-9])|([1-9][0-9])|[1-9])$');
-    
-    if() {
-       
-    } else {
-       
+    if (re.test(document.getElementById('email').value)) {
+        email.style.backgroundColor="white";
     }
-}
+    else{
+        email.style.backgroundColor="red";
+    }
+
+};
+
+document.getElementById('age').onkeyup = function() {
+    var age = document.getElementById('age');
+    var regEx = new RegExp('^(([0-9][1-9])|([1-9][0-9])|[1-9])$');
+
+    if(regEx.test(document.getElementById('age').value)) {
+            age.style.backgroundColor="white";
+    } else {
+            age.style.backgroundColor="red";
+    }
+};
+
+
+document.getElementById('name').onkeyup= function() {
+    var name = document.getElementById('name');
+    var regExp = new RegExp('<');
+
+    if(regExp.test(document.getElementById('name').value)) {
+        name.style.backgroundColor="red";
+    }
+    else{
+        name.style.backgroundColor="white";
+    }
+};
